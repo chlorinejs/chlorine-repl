eff474455aa2aa602a17711ffc5113be44d9d0f6
Hoang Minh Thang <p@banphim.net>
2013-04-12 16:38:48 +0700

    node-eval works
    --HG--
    branch : node
    extra : amend_source : 50f96bc5b296155b651f53d9fc74b4214344a653

--- src/chlorine/node.clj
+++ src/chlorine/node.clj
@@ -1,19 +1,16 @@
 (ns chlorine.node
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
-            [cljs.analyzer :as ana]
-            [cljs.repl :as repl]
             [cheshire.core :refer [parse-string generate-string]]
             [chlorine.repl :as cl2-repl])
-  (:import cljs.repl.IJavaScriptEnv
-           java.io.PipedReader
+  (:import java.io.PipedReader
            java.io.PipedWriter))
 
 (defn- load-as-tempfile
   "Copy a file from the classpath into a temporary file.
   Return the path to the temporary file."
   [filename]
-  (let [tempfile (java.io.File/createTempFile "cljsrepl" ".js")
+  (let [tempfile (java.io.File/createTempFile "cl2repl" ".js")
         resource (io/resource filename)]
     (.deleteOnExit tempfile)
     (assert resource (str "Can't find " filename " in classpath"))
@@ -46,7 +43,7 @@
   (try (.exitValue process) false
        (catch IllegalThreadStateException e true)))
 
-(defn- launch-node-process
+(defn launch-node-process
   "Launch the Node subprocess."
   [& [output-func]]
   ;; Launch repl.js through an eval to trick Node into thinking it was
@@ -65,79 +62,22 @@
      :output (io/writer (.getOutputStream process))
      :loaded-libs (atom #{})}))
 
-(defn js-eval [env filename line code]
-  (let [{:keys [input output]} env]
-    (.write output (str (generate-string {:file filename :line line :code code})
-                        "\n"))
-    (.flush output)
-    (let [result-string (.readLine input)]
-      (parse-string result-string true))))
-
-(defn node-setup [repl-env]
-  (let [env (ana/empty-env)]
-    (repl/load-file repl-env "cljs/core.cljs")
-    (swap! (:loaded-libs repl-env) conj "cljs.core")
-    (repl/evaluate-form repl-env env "<cljs repl>"
-                        '(ns cljs.user))
-    (repl/evaluate-form repl-env env "<cljs repl>"
-                        '(set! cljs.core/*print-fn* (.-print (js/require "util"))))))
-
-(defn node-eval [repl-env filename line js]
-  (let [result (js-eval repl-env filename line js)]
+(defn node-eval [repl-env expr]
+  (let [result
+        (let [{:keys [input output]} repl-env]
+          (.write output (str (generate-string {:file "nofile.js"
+                                                :line 0
+                                                :code expr})
+                              "\n"))
+          (.flush output)
+          (let [result-string (.readLine input)]
+            (parse-string result-string true)))]
     (if-let [error (:error result)]
       {:status :exception :value (:stack error)}
       {:status :success :value (:result result)})))
 
-(defn load-javascript [repl-env ns url]
-  (let [missing (remove #(contains? @(:loaded-libs repl-env) %) ns)]
-    (when (seq missing)
-      (js-eval repl-env (.toString url) 1 (slurp url))
-      (swap! (:loaded-libs repl-env) (partial apply conj) missing))))
-
 (defn node-tear-down [repl-env]
-  (let [process (:process repl-env)]
-    (doto process
-      (.destroy)
-      (.waitFor))))
-
-(defn load-resource
-  "Load a JS file from the classpath into the REPL environment."
-  [env filename]
-  (let [resource (io/resource filename)]
-    (assert resource (str "Can't find " filename " in classpath"))
-    (js-eval env filename 1 (slurp resource))))
-
-(defrecord NodeEnv []
-  repl/IJavaScriptEnv
-  (-setup [this]
-    (node-setup this))
-  (-evaluate [this filename line js]
-    (node-eval this filename line js))
-  (-load [this ns url]
-    (load-javascript this ns url))
-  (-tear-down [this]
-    (node-tear-down this)))
-
-(defn repl-env
-  "Create a Node.js REPL environment."
-  [& {:as opts}]
-  (let [base (io/resource "goog/base.js")
-        deps (io/resource "goog/deps.js")
-        process (launch-node-process (:output opts))
-        new-repl-env (merge (NodeEnv.)
-                            (merge process
-                                   {:optimizations :simple}))]
-    (assert base "Can't find goog/base.js in classpath")
-    (assert deps "Can't find goog/deps.js in classpath")
-    (load-resource new-repl-env "goog/base.js")
-    (load-resource new-repl-env "goog/deps.js")
-    new-repl-env))
-
-(defn run-node-repl []
-  (repl/repl (repl-env)))
-
-(defn nrepl-env []
-  (doto (repl-env) (node-setup)))
-
-(defn run-node-nrepl []
-  (cl2-repl/cl2-repl :repl-env (nrepl-env)))
+   (let [process (:process repl-env)]
+     (doto process
+       (.destroy)
+       (.waitFor))))
