3d58c4804bdb783fcd70d2485500352e4c33e64c
Hoàng Minh Thắng <p@banphim.net>
2013-05-30 21:28:47 +0700

    update dependencies and information

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject chlorine-repl "0.0.3"
+(defproject chlorine-repl "0.0.4"
   :description "Running Chlorine REPLs over nREPL."
-  :url "http://github.com/myguidingstar/chlorine-repl"
+  :url "http://github.com/chlorinejs/chlorine-repl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.0-RC1"]
-                 [chlorine "1.5.3"]
-                 [core-cl2 "0.7.1"]
+                 [chlorine "1.5.3.1"]
+                 [core-cl2 "0.7.2"]
                  [myguidingstar/clansi "1.3.0"]]
 
   :injections [(require 'chlorine.repl)]
   :repl-options {:nrepl-middleware [chlorine.repl/wrap-chlorine-repl]}
 
-  :scm {:url "git@github.com:chlorine/chlorine-repl.git"})
+  :scm {:url "git@github.com:chlorinejs/chlorine-repl.git"})
