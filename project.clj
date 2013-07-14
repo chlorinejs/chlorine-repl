c8e0571167c065a55a0409c4ad09a7f24087ac1c
Hoàng Minh Thắng <p@banphim.net>
2013-07-15 02:22:40 +0700

    update chlorine

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.0-RC1"]
-                 [chlorine "1.5.4"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [chlorine "1.6.0"]
                  [core-cl2 "0.7.3"]
                  [myguidingstar/clansi "1.3.0"]]
 
