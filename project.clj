25cbab49614725d72a4cf669cdca3ab73d9e5575
Hoàng Minh Thắng <p@banphim.net>
2013-07-18 18:26:28 +0700

    update chlorine and core-cl2

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [chlorine "1.6.0"]
-                 [core-cl2 "0.7.3"]
+                 [chlorine "1.6.1"]
+                 [core-cl2 "0.8.0"]
                  [myguidingstar/clansi "1.3.0"]]
 
   :injections [(require 'chlorine.repl)]
