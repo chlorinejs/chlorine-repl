650726eec7588e4b6dad8dd8e2efd99fcbced543
Hoàng Minh Thắng <p@banphim.net>
2013-11-11 01:09:36 +0700

    bump to v0.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject chlorine-repl "0.0.5"
+(defproject chlorine-repl "0.1.0-SNAPSHOT"
   :description "Running Chlorine REPLs over nREPL."
   :url "http://github.com/chlorinejs/chlorine-repl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.2.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
-                 [chlorine "1.6.2"]
-                 [core-cl2 "0.8.0"]
+                 [chlorine "1.6.4-SNAPSHOT"]
+                 [core-cl2 "0.9.0-SNAPSHOT"]
                  [myguidingstar/clansi "1.3.0"]]
 
   :injections [(require 'chlorine.repl)]
